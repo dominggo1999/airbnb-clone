7ce64a465fc02639b77de6a3d5d09210a72cb53e
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-03 16:38:00 +0700

    starter template

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import tw from 'twin.macro';
+
+const index = () => {
+  return (
+    <div tw="bg-black w-full h-screen text-4xl flex flex-col justify-center items-center text-white">
+      Hellow
+    </div>
+  );
+};
+
+export default index;
